6b481d00027b94fe54a89da779b99b5169854d38
Unknown <iansuddreth@gmail.com>
2017-03-16 11:56:36 -0600

    adding terrian to castle

---
+++ Castle/Assets/HealthTest.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class HealthTest : MonoBehaviour
+{
+    
+
+    // START FUNCTION
+    void Start()
+    {
+    }
+
+    // UPDATE FUNCTION
+    void Update()
+    {
+
+    }
+
+    
+
+    // GUI FUNCTION
+    void OnGUI()
+    {
+        //Access the 'InReach' variable from raycasting script.
+        GameObject Player = GameObject.Find("Player");
+        Detection detection = Player.GetComponent<Detection>();
+
+        if (detection.InReach == true)
+        {
+            GUI.color = Color.white;
+            GUI.Box(new Rect(20, 20, 200, 25), "Health Test Cube");
+        }
+    }
+}
