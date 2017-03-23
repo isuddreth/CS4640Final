a91bbfaf77f14f491e556f051bf039b726bf3997
Unknown <abingsa1@gmail.com>
2017-03-23 13:05:07 -0600

    added crossbow and it shoots

---
+++ Assets/CrossBow stuff/bz07k0njy8-Crossbow/myarrow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myarrow : MonoBehaviour 
+{
+	public float speed = 10;
+
+
+	void Update ()
+	{
+		this.transform.Translate (Vector3.forward * speed * Time.deltaTime);
+
+	}
+}
