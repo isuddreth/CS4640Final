b2cdbaeb279ac0b7fb5673a49bcc548a8644496a
Unknown <abingsa1@gmail.com>
2017-04-04 12:18:29 -0600

    arrows have colliders

---
+++ Assets/DeerHit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeerHit : MonoBehaviour {
+
+	public GameObject deer;
+	public bool hit = false;
+	public float lifetime = 5f;
+	// Use this for initialization
+	void Start () {
+		//transform.localEulerAngles = new Vector3(360,-180,90);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (hit)
+
+		{
+			Destroy (deer, lifetime);
+		}
+	}
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		deer.transform.Rotate(0, 0, -90);
+		hit = true;
+
+	}
+
+
+}
