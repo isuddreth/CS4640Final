5e857fc1f425d78c6d9be849d00862821172dac0
Unknown <iansuddreth@gmail.com>
2017-04-06 11:54:24 -0600

    added question manager

---
+++ Assets/Scripts/Managers/QuestManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour {
+
+    public static QuestManager questManager;
+
+    public List<Quest> questList = new List<Quest>(); // master list
+    public List<Quest> curentQuests = new List<Quest>(); // current quests
+
+    // private vars for our quests
+
+    private void Awake()
+    {
+        if (questManager == null)
+        {
+            questManager = this;
+        }
+        else if (questManager != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    
+}
